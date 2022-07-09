fa2ded383b8a871aafcabe3833b921b16e300741
Adauto Júnior <adautodcjunior@gmail.com>
2022-07-09 14:20:07 -0300

    feat: adding specialty model

---
+++ src/database/models/SpecialtyModel.ts
@@ -0,0 +1,30 @@
+import { db } from '..';
+import { DataTypes, Model } from 'sequelize';
+
+export type specialtyType = {
+  id: number;
+  name: string;
+};
+
+export const SpecialtyModel = class Specialty extends Model<specialtyType> {};
+
+SpecialtyModel.init(
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'Specialty',
+    tableName: 'specialty',
+    timestamps: false,
+  }
+);
