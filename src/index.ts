f107083641101e664284a382ead168f5a360462e
Adauto Júnior <adautodcjunior@gmail.com>
2022-07-13 21:34:55 -0300

    Finishing class 01

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import swagger from 'swagger-ui-express';
 import { db } from './database';
 import { routes } from './routes';
+import swaggerDoc from '../swagger.json';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(json());
 app.use(cors());
+
+app.use('/api/docs', swagger.serve, swagger.setup(swaggerDoc));
 
 routes(app);
 
